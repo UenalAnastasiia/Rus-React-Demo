69f013cd4d2bd1f1e63acbd0f9ad5765480ad55d
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-06-23 13:50:08 +0200

    add post list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/App.css';
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript 1', body: 'Description 1' },
+    { id: 2, title: 'Javascript 2', body: 'Description 2' },
+    { id: 3, title: 'Javascript 3', body: 'Description 3' }
+  ])
 
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python 1', body: 'Description 1' },
+    { id: 2, title: 'Python 2', body: 'Description 2' },
+    { id: 3, title: 'Python 3', body: 'Description 3' }
+  ])
+
+  
   return (
     <div className="App">
-      <PostItem value={'2222'} item={{title: 0}} number={1} />
+      <PostList posts={posts} title="Posts über Javascript" />
+      <PostList posts={posts2} title="Posts über Python" />
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import '../styles/App.css';
 
 const PostItem = (props) => {
-    console.log(props);
-    
+
     return (
         <div>
             <div className="App">
                 <div className="post">
                     <div className="post__content">
-                        <strong>1. Javascript</strong>
-                        <div>Javascript ist eine Programmiersprache</div>
+                        <strong>{props.post.id}. {props.post.title}</strong>
+                        <div>{props.post.body}</div>
                     </div>
 
                     <div>
