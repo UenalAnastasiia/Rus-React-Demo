02dda9e95ce1a491579ab8b5de020179f9e1a38b
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-06-23 15:05:43 +0200

    add useRef to controlled and uncontrolled input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -8,18 +10,36 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description 2' },
     { id: 3, title: 'Javascript 3', body: 'Description 3' }
   ])
+  const [title, setTitle] = useState('');
+  const bodyInputRef = useRef();
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python 1', body: 'Description 1' },
-    { id: 2, title: 'Python 2', body: 'Description 2' },
-    { id: 3, title: 'Python 3', body: 'Description 3' }
-  ])
+  const addNewPost = (e) => {
+    e.preventDefault();     // Stop page reload
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  }
 
-  
+
   return (
     <div className="App">
+      <form>
+        {/* controlled Input */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Post Title" />
+
+        {/* uncontrolled Input */}
+        <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Post Description" />
+
+        <MyButton onClick={addNewPost}>Post erstellen</MyButton>
+      </form>
+
       <PostList posts={posts} title="Posts über Javascript" />
-      <PostList posts={posts2} title="Posts über Python" />
     </div>
   );
 }
