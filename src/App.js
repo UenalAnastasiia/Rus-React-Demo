a745569617c30704d26a974cd4fec66b32c5a098
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-06-27 20:40:46 +0200

    useEffect for load data by start, axios - use api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,6 +6,7 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -14,9 +15,20 @@ function App() {
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
 
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
+  }
+
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(response.data);
   }
 
 
@@ -27,7 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: '30px'}}
+      <MyButton style={{ marginTop: '30px' }}
         onClick={() => setModal(true)}>
         Post erstellen
       </MyButton>
